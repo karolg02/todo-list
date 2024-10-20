e6dc5a9d01bc097511d4c62aa2bebd780380c7e2
karolg02 <karolglanowski02@gmail.com>
2024-10-20 19:54:48 +0200

    database connection + filtering

--- backend-todo-list/src/main.ts
+++ backend-todo-list/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, //conversion of types
+      whitelist: true,  //remove properties not present in DTO
+      forbidNonWhitelisted: true,
+    })
+  );
+
   await app.listen(process.env.PORT ?? 9000);
 }
 bootstrap();
--- backend-todo-list/src/modules/todo/todo.controller.ts
+++ backend-todo-list/src/modules/todo/todo.controller.ts
@@ -1,4 +1,60 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { TodoService } from './todo.service';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { TodoNotfoundException } from '../../exceptions/todo-notfound-exception';
+import { EditTodoDto } from './dto/edit-todo.dto';
+import { TodoFilterDto } from './dto/todo-filter.dto';
 
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+
+  constructor(private todoService: TodoService) {
+  }
+
+  @Get()
+  listTodo(@Query() filter: TodoFilterDto){
+    return this.todoService.listTodo(filter);
+  }
+
+  @Get(":id")
+  async getTodo(@Param("id", ParseIntPipe) id:number){
+    const todo = await this.todoService.get(id);
+    if(!todo) throw new TodoNotfoundException();
+
+    return todo;
+  }
+
+  @Post()
+  addTodo(@Body() data: CreateTodoDto){
+    return this.todoService.addTodo(data);
+  }
+
+  @Delete(":id")
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async deleteTodo(@Param("id", ParseIntPipe) id:number){
+    const todo = await this.todoService.get(id);
+    if(!todo) throw new TodoNotfoundException();
+
+    await this.todoService.deleteToDo(id);
+  }
+
+  @Put(":id")
+  async editTodo(@Param("id", ParseIntPipe) id:number, @Body() data: EditTodoDto){
+    const todo = await this.todoService.get(id);
+    if(!todo) throw new TodoNotfoundException();
+
+    return this.todoService.editTodo(id, data);
+  }
+}
